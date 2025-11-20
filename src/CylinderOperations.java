f2ebd34c7c8a22ea1317b7e0d82996d77d3abb05
ash <getseth00@gmail.com>
2025-11-20 20:23:57 +0530

    daily update

---
+++ src/CylinderOperations.java
@@ -0,0 +1,22 @@
+
+class cylinder{
+    int height;
+    int radius;
+    public int getRadius(){
+        return this.radius;
+    }
+    public void setRadius(int r){
+        this.radius =r;
+    }public int getHeight(){
+        return this.height;
+    }
+    public void setHeight(int h){
+        this.height=h;
+    }
+}
+
+public class CylinderOperations {
+    public static void main(String[] args) {
+        
+    }
+}
